422faecfcb29c654d15c847848bbf8c0fdaa658c
Ana Meisel <anameisel@Anas-MacBook-Air.local>
2024-03-04 10:41:09 +0000

    added update blink and force reload on gif

--- src/App.js
+++ src/App.js
@@ -11,26 +11,25 @@ function App() {
   const [data, setData] = useState([]);
   const [clickedData, setClickedData] = useState([])
   const [clickedDataFive, setClickedDataFive] = useState([])
-  const [gifPlayed, setGifPlayed] = useState(false);
+  const [blink, setBlink] = useState("visible")
+  const [blinkGifUrl, setBlinkGifUrl] = useState('./output-onlinegiftools.gif')
 
-  const apikey = 'a8ec3adf11a71a6e3ef495d42c8fb9f4';
+    const apikey = 'a8ec3adf11a71a6e3ef495d42c8fb9f4';
     const category = 'world';
     const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=50&apikey=' + apikey;
     useEffect(() => {
      fetch(url).then(result => result.json()).then(alldata => setData(alldata.articles)).catch(error => console.log(error.message))
     }, [])
 
-    function nextQuestion() {
-      if (!gifPlayed) {
-        const blinkElement = document.getElementById('blink');
-        if (blinkElement) {
-          blinkElement.onload = () => {
-            blinkElement.src = blinkGif;
-            setGifPlayed(true);
-          };
-          blinkElement.src = blinkGif; 
-        }
-      }
+    function nextData() {
+      setBlink("hidden");
+      setTimeout(() => {
+        setBlink("visible");
+      }, 250);
+
+      // force reload on gif
+      const randomQuery = Math.random();
+      setBlinkGifUrl(`./output-onlinegiftools.gif?${randomQuery}`);
 
       const nextIndex = currentData >= data.length - 1 ? 0 : currentData + 1;
       setCurrentData(nextIndex);
@@ -56,12 +55,12 @@ function App() {
   return (
     <div className="App">
       <div className="tabloid">
-          <div onClick={() => nextQuestion()} className="blink-button">
+          <div onClick={() => nextData()} className="blink-button">
             <div className="title">Blink to update: </div>
-            <button className="blink-img" onClick={nextQuestion}><img id="blink" src={blinkGif} alt="Blinking eye gif"></img></button>
+            <button className="blink-img" onClick={nextData}><img id="blink" src={blinkGif} alt="Blinking eye gif"></img></button>
             <div className="title2">Click my eye!!</div>
           </div>
-            <div className="answers"> 
+            <div style={{ visibility: blink }} className="answers"> 
               {data[currentData] && (
                 <div className="li" key={data[currentData].publishedAt}>{data[currentData].title}
                 <div className="gif"><GifDemo /></div>
